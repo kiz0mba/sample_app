299227265f2018a8e2b793c6a1b0ef1310bb12fe
kizumba <guilhermevarela@hotmail.com>
2013-05-27 19:10:03 -0300

    Advanced Setup and Extra-Credit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
+	
+   let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home Page" do
     it "Should have the content 'Sample App'" do
 	visit '/static_pages/home'
@@ -11,7 +12,7 @@ describe "StaticPages" do
    it "Should have the right title" do
 	visit '/static_pages/home'
       	page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | Home")  
+			:text => "#{base_title} | Home")  
     end
   end
 
@@ -23,7 +24,7 @@ describe "StaticPages" do
     it "Should have the right title" do
 	visit '/static_pages/help'
       	page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | Help")  
+			:text => "#{base_title} | Help")  
     end
   end
   describe "About Page" do
@@ -34,7 +35,18 @@ describe "StaticPages" do
  	it "Should have the right title" do
 		visit '/static_pages/about'
       		page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | About Us")  
+			:text => "#{base_title} | About Us")  
+    	end
+  end
+ describe "Contact Page" do
+ 	it "Should have the content 'Contact'" do
+		visit '/static_pages/contact'
+      		page.should have_content('Contact')  
+    	end
+ 	it "Should have the right title" do
+		visit '/static_pages/contact'
+      		page.should have_selector('title',
+			:text => "#{base_title} | Contact")  
     	end
   end
 end

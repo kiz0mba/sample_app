4b2fc7604ffc284a6440a1c1f5a8511bab1eb54e
kizumba <guilhermevarela@hotmail.com>
2013-06-01 18:50:41 -0300

    Added chapter5-exercices contents

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,55 +2,72 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-   subject { page } #Capybara: Tells that this is the subject of tests
+  subject { page } #Capybara: Tells that this is the subject of tests
+  
+  shared_examples_for "all static pages" do 
+    it { should have_selector('h1', text: heading ) }
+    it { should have_selector('title', text: full_title( page_title ) ) }
+  end
 	
    #let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home Page" do
-  
     before { visit root_path }
-	
-    it { should have_selector('h1',text:'Sample App') } 
     
-    it { should have_selector('title',:text => full_title("")) }
- 
-    it { should_not have_selector('title',:text => "| Home")  }
+    let(:heading) { 'Sample App' }
 
+    let(:page_title) { '' }	
+    
+    it_should_behave_like "all static pages"
+ 
   end
 
   describe "Help Page" do
 
-    before { visit help_page}
+    before { visit help_path }
 
-    it { should have_selector('h1','Help') }
+    let(:heading) { 'Help' }
+
+    let(:page_title) { '' }	
     
-    it { should have_selector('title',text: full_title(""))}
-
-    it { should_not have_selector('title',text :"| Help")}
-
+    it_should_behave_like "all static pages"
+ 
   end
 
   describe "About Page" do
 
-	before {visit about_page}
+    before {visit about_path }
 
- 	it { should have_selector('h1','About Us') }
+    let(:heading) { 'About' }
 
- 	it { should have_selector('title', text: full_title("")) }
-
-	it { should_not have_selector('title',text: "| About")  }
-    	
+    let(:page_title) { '' }	
+    
+    it_should_behave_like "all static pages"
+ 
   end
- describe "Contact Page" do
+  describe "Contact Page" do
 
-	before {visit contact_page}
+    before { visit contact_path }
  	
-	it { should have_selector('h1','Contact') }
+    let(:heading) { 'Contact' }
 
- 	it { should have_selector('title',text:full_title("")) }
-
-	it { should_not have_selector('title',
-				:text => "| Contact")}
+    let(:page_title) { '' }	
+    
+    it_should_behave_like "all static pages"
+  
   end
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title',text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title',text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title',text: full_title('Contact')
+    click_link "Home"
+    page.should have_selector 'title',text: full_title('Home')
+    click_link "Sign up now!"
+    page.should have_selector 'title',text: full_title('Sign up')
 
+  end
 end
-end
+

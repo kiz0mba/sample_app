da625719205da28d5c91cb75f85d20986e948818
kizumba <guilhermevarela@hotmail.com>
2013-06-15 20:45:28 -0300

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,24 +49,51 @@ describe "Authentication" do
 
 
     describe "for non-signed in users" do
-      let(:user)  { FactoryGirl.create(:user) }
+      let(:user) { FactoryGirl.create(:user) }
 
-      describe "when attempting to visit a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email", with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
+      describe "in the Users controller" do
         
-        describe "after signing in" do
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
 
-          it "should render the desired protected page" do
-            page.should have_selector('title', text: 'Edit user')
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify {response.should redirect_to(signin_path)}
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "when attempting to visit a protected page" do
+          before do
+            visit edit_user_path(user)
+            fill_in "Email", with: user.email
+            fill_in "Password", with: user.password
+            click_button "Sign in"
+          end
+        
+          describe "after signing in" do
+
+            it "should render the desired protected page" do
+              page.should have_selector('title', text: 'Edit user')
+            end
+          end
+          
+          describe "vising the following page" do
+            before { visit following_user_path(user) }
+            it { should have_selector('title', text: 'Sign in') } 
+          end
+      
+          describe "visiting the follower page" do
+            before {  visit followers_user_path(user) }
+            it { should have_selector('title', text: 'Sign in') }
           end
         end
       end
-
       describe "in the Microposts controller" do
         
         describe "submitting to the create action" do
@@ -82,25 +109,19 @@ describe "Authentication" do
           specify { response.should redirect_to(signin_path) }
         end
       end
+
+      describe "in the Relationships controller" do
+        
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+        describe "submitting to the destroy action" do
+          before { delete relationships_path(1) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end 
     end
-    describe "in the Users controller" do     
-      let(:user)  { FactoryGirl.create(:user) }
-
-      describe "visiting the edit page" do
-        before {  visit edit_user_path(user) }
-        it { should have_selector('title', text: 'Sign in') }
-      end
-
-      describe "submitting to the update action" do
-        before { put user_path(user) }
-        specify {response.should redirect_to(signin_path)}
-      end
-
-      describe "visiting the user index" do
-        before { visit users_path }
-        it { should have_selector('title', text: 'Sign in') }
-      end
-    end    
   end
  
   describe "as wrong user" do
@@ -111,7 +132,7 @@ describe "Authentication" do
     describe "visiting Users#edit page" do
       before { visit edit_user_path(wrong_user) }
         it { should_not have_selector('title', text: full_title('Edit user')) }
-    end  
+    end
   
     describe "submitting a PUT request to the Users#update action" do
       before { put user_path(wrong_user) }
@@ -119,7 +140,7 @@ describe "Authentication" do
     end
   end
   describe "as non-admin user" do
-    let(:user)      { FactoryGirl.create(:user) } 
+    let(:user) { FactoryGirl.create(:user) }
     let(:non_admin) { FactoryGirl.create(:user) }
 
     before { sign_in non_admin }
